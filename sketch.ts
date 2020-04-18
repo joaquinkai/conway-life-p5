3c2016c17e7f00343142b2201b10d7ec92a9b617
Dave Briccetti <daveb@davebsoft.com>
2020-04-18 15:19:56 -0700

    Try a 3D rendering

--- sketch.ts
+++ sketch.ts
@@ -37,17 +37,28 @@ new p5(p => {
   p.draw = () => {
     p.frameRate(2);
     const topMargin = 100;
+    const leftMargin = 50;
+    p.background('white');
+    p.rotateX(p.TAU / 8);
     p.translate(-p.width / 2, -p.height / 2);
-    const cellHeight = p.height / ROWS;
-    const cellWidth = p.width / COLS;
+    const cellHeight = (p.height - topMargin) / ROWS;
+    const cellWidth = (p.width - leftMargin) / COLS;
     const cellSize = p.min(cellHeight, cellWidth);
+    const fills = [[0, 0, 255, 150], [240, 240, 240, 255]];
+    const strokes = ['black', 'lightgray'];
+
     for (let row = 0; row < ROWS; ++row) {
       for (let col = 0; col < COLS; ++col) {
-        p.strokeWeight(5);
-        p.stroke('white');
-        p.fill(grid.get(row, col) ? 'purple' : 'white');
-        p.rect(col * cellSize, topMargin + row * cellSize,
-          cellSize, cellSize);
+        const alive = grid.get(row, col);
+        const aliveIndex = alive ? 0 : 1;
+        const stroke = strokes[aliveIndex];
+        if (stroke) p.stroke(stroke); else p.noStroke();
+        p.fill(...fills[aliveIndex]);
+        p.push();
+        p.translate(leftMargin + col * cellSize, topMargin + row * cellSize, 0);
+        const s = cellSize * 0.8;
+        p.box(s, s, alive ? s / 3 : 0);
+        p.pop();
       }
     }
     grid.advance();
